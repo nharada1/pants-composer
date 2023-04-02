ba325a356f55267c6415f0f924ca0fa6893016b7
Nate Harada <nharada1@gmail.com>
2023-04-02 10:53:45 -0700

    Adding more files

--- src/train.py
+++ src/train.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+import numpy as np
+
 import torch
 import torch.utils.data
 import torch.nn.functional as F
@@ -8,36 +12,65 @@ from composer.models import ComposerModel
 from composer import Trainer
 from composer.utils import dist
 
+from streaming import StreamingDataset
 
 class ResNet18(ComposerModel):
     def __init__(self):
         super().__init__()
-        self.model = torchvision.models.resnet18()
+        self.model = torchvision.models.resnet50(num_classes=63)
         self.optim = torch.optim.Adam(self.model.parameters(), lr=0.01)
 
     def forward(self, batch):
         inputs, _ = batch
+        inputs = inputs[:, 0:3, :, :]
         return self.model(inputs)
 
     def loss(self, outputs, batch):
         _, targets = batch
-        return F.cross_entropy(outputs, targets)
+        oh = F.one_hot(targets, num_classes=63)
+        oh = oh.squeeze().float()
+        return F.cross_entropy(outputs, oh)
+
+
+class CustomDataset(StreamingDataset):
+   def __init__(self, local, remote, transform):
+       super().__init__(local=local, remote=remote)
+       self.transform = transform
+
+   def __getitem__(self, idx: int) -> Any:
+        obj = super().__getitem__(idx)
+        image, label = obj["image"], obj["label"]
+
+        if self.transform:
+            image = self.transform(image.astype(np.float32))
+
+        return image, label
 
 
 def main():
-    datadir = "./data"
-    batch_size = 1024
+    batch_size = 32
+    mean=np.array(
+        [349.23, 339.76, 378.58, 418.42, 275.86, 431.82, 495.65, 435.05]
+    )
+    std=np.array(
+        [78.67, 105.54, 142.05, 177.00, 132.29, 151.65, 194.00, 166.27]
+    )
 
     transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        [transforms.ToTensor(), transforms.Resize((256, 256)), transforms.Normalize(mean, std)]
     )
+    local_dir = "/home/nharada/Datasets/remote_sensing/materialized/mds/fmow_full_resized/val"
+    remote_dir = None
 
-    trainset = torchvision.datasets.CIFAR10(
-        root=datadir, train=True, download=True, transform=transform
+    trainset = CustomDataset(local=local_dir, remote=remote_dir, transform=transform)
+    '''
+    transform = transforms.Compose(
+        [transforms.ToTensor()]
     )
-    sampler = dist.get_sampler(trainset, shuffle=True)
+    trainset = torchvision.datasets.FakeData(size=100000, image_size=(3, 256, 256), num_classes=63, transform=transform)
+    '''
     train_loader = torch.utils.data.DataLoader(
-        trainset, sampler=sampler, batch_size=batch_size, num_workers=2
+        trainset, batch_size=batch_size, num_workers=8
     )
 
     model = ResNet18()
